25c9675b86efe2106a1d47ddfcdb2d936fb6a92c
Robert O'Shea <robertoshea2k11@gmail.com>
2024-01-06 04:55:04 +0000

    Fixed issue with formatting at save

--- lua/robertos/plugins/conform.lua
+++ lua/robertos/plugins/conform.lua
@@ -1,30 +1,36 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local conform = require("conform")
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        local conform = require("conform")
 
-		conform.setup({
-			formatters_by_ft = {
-				lua = { "stylua" },
-				javascript = { "prettier" },
-				c = { "clang-format" },
-				rust = { "ast-grep" },
-			},
+        conform.setup({
+            formatters_by_ft = {
+                lua = { "stylua" },
+                javascript = { "prettier" },
+                c = { "clang-format" },
+                rust = { "ast-grep" },
+            },
+        })
 
-			format_on_save = {
-				lsp_fallback = "always",
-				async = false,
-				timeout_ms = 500,
-			},
-		})
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            pattern = "*",
+            callback = function(args)
+                local old_location = vim.api.nvim_win_get_cursor(0)
+                require("conform").format({
+                    lsp_fallback = "always",
+                    bufnr = args.bufnr,
+                })
+                pcall(vim.api.nvim_win_set_cursor, 0, old_location)
+            end,
+        })
 
-		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-			conform.format({
-				lsp_fallback = "always",
-				async = false,
-				timeout_ms = 500,
-			})
-		end, { desc = "Format file or range (in visual mode)" })
-	end,
+        vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+            conform.format({
+                lsp_fallback = "always",
+                async = false,
+                timeout_ms = 500,
+            })
+        end, { desc = "Format file or range (in visual mode)" })
+    end,
 }
