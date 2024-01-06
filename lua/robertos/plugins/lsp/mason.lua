263862226edc8c9cbe087a0c739a554cd2efafd4
Robert O'Shea <robertoshea2k11@gmail.com>
2024-01-06 04:53:40 +0000

    Changed settings of Lsps

--- lua/robertos/plugins/lsp/mason.lua
+++ lua/robertos/plugins/lsp/mason.lua
@@ -1,37 +1,40 @@
 return {
-	"williamboman/mason.nvim",
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-	},
-	config = function()
-		local mason = require("mason")
-		local mason_lspconfig = require("mason-lspconfig")
-		local mason_tool_installer = require("mason-tool-installer")
+    "williamboman/mason.nvim",
+    dependencies = {
+        "williamboman/mason-lspconfig.nvim",
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+    },
+    config = function()
+        local mason = require("mason")
+        local mason_lspconfig = require("mason-lspconfig")
+        local mason_tool_installer = require("mason-tool-installer")
 
-		mason.setup({})
-		mason_lspconfig.setup({
-			ensure_installed = {
-				"lua_ls",
-				"pyright",
-				"eslint",
-				"rust_analyzer",
-				"tsserver",
-				"clojure_lsp",
-				"clangd",
-			},
-			automatic_installation = true,
-		})
-		mason_tool_installer.setup({
-			ensure_installed = {
-				"black",
-				"stylua",
-				"pylint",
-				"isort",
-				"prettier",
-				"clang-format",
-				"ast-grep",
-			},
-		})
-	end,
+        mason.setup({})
+        mason_lspconfig.setup({
+            ensure_installed = {
+                "lua_ls",
+                "pyright",
+                "eslint",
+                "rust_analyzer",
+                "tsserver",
+                "clojure_lsp",
+                "clangd",
+                "phpactor",
+            },
+            automatic_installation = true,
+        })
+        mason_tool_installer.setup({
+            ensure_installed = {
+                "black",
+                "stylua",
+                "pylint",
+                "isort",
+                "prettier",
+                "clang-format",
+                "ast-grep",
+                "phpcs",
+                "php-cs-fixer",
+            },
+        })
+    end,
 }
