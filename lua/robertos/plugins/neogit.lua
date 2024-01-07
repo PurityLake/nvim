f8ded810ad12ebb7110de9bb471b716740c35cec
Robert O'Shea <robertoshea2k11@gmail.com>
2024-01-07 04:38:54 +0000

    Customised Neogit

--- lua/robertos/plugins/neogit.lua
+++ lua/robertos/plugins/neogit.lua
@@ -5,5 +5,35 @@ return {
 		"sindrets/diffview.nvim",
 		"nvim-telescope/telescope.nvim",
 	},
-	config = true,
+	config = function()
+		local neogit = require("neogit")
+		neogit.setup({})
+
+		local old_open = neogit.open
+		local old_close = neogit.close
+
+		---@diagnostic disable-next-line: duplicate-set-field
+		neogit.open = function(...)
+			old_open(...)
+			vim.keymap.set("n", "<leader>gq", function()
+				require("neogit").close()
+			end, { desc = "Close Neogit if it is open already" })
+		end
+
+		neogit.close = function(...)
+			old_close(...)
+			vim.keymap.del("n", "<leader>gq")
+		end
+
+		vim.keymap.set("n", "<leader>gg", function()
+			require("neogit").open()
+		end, { desc = "Open Neogit" })
+
+		local group = vim.api.nvim_create_augroup("MyNeogitEvents", { clear = true })
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "NeogitPushComplete",
+			group = group,
+			callback = require("neogit").close,
+		})
+	end,
 }
