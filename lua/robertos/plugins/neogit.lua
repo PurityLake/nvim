859e0352bae2893bad61b2171d10a247e5155575
Robert O'Shea <robertoshea2k11@gmail.com>
2024-01-08 01:48:40 +0000

    Remove pointless code

--- lua/robertos/plugins/neogit.lua
+++ lua/robertos/plugins/neogit.lua
@@ -9,25 +9,13 @@ return {
 		local neogit = require("neogit")
 		neogit.setup({})
 
-		local old_open = neogit.open
-		local old_close = neogit.close
-
-		---@diagnostic disable-next-line: duplicate-set-field
-		neogit.open = function(...)
-			old_open(...)
-			vim.keymap.set("n", "<leader>gq", function()
-				require("neogit").close()
-			end, { desc = "Close Neogit if it is open already" })
-		end
-
-		neogit.close = function(...)
-			old_close(...)
-		end
-
 		vim.keymap.set("n", "<leader>gg", function()
 			require("neogit").open()
 		end, { desc = "Open Neogit" })
 
+		vim.keymap.set("n", "<leader>gq", function()
+			require("neogit").close()
+		end, { desc = "Close Neogit" })
 		local group = vim.api.nvim_create_augroup("MyNeogitEvents", { clear = true })
 		vim.api.nvim_create_autocmd("User", {
 			pattern = "NeogitPushComplete",
